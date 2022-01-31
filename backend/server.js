5f482a27174844a1f9c76622b21358053ffe3b8e
Fanny Reslow <reslowf@gmail.com>
2022-01-31 21:26:40 +0100

    installing cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.static("../frontend"));
 app.use(express.json());
+app.use(cookieParser());
 
+let accounts = [];
 app.post("/api/auth/create", (req, res) => {
   const account = req.body;
   console.log("SIGNING UP");
 
-  res.json("hej");
+  let responsObject = {
+    success: false,
+  };
+
+  if (account) {
+    responsObject = true;
+  }
+
+  res.json(responsObject);
 });
 
 app.listen(3000, () => {
   console.log("server listen on port 3000");
 });
+ç;
