3390015f30e338f1a6762ffa5c087c24b31e279d
Fanny Reslow <reslowf@gmail.com>
2022-02-01 17:36:34 +0100

    create, remove account, login and logout is working

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,79 @@ app.post("/api/auth/create", (req, res) => {
   } else {
     accounts.push(credentials);
   }
+
+  res.json(responsObject);
+});
+
+app.post("/api/auth/login", (req, res) => {
+  const credentials = req.body;
+
+  let responsObject = {
+    success: false,
+  };
+
+  accounts.forEach((account) => {
+    if (
+      account.username == credentials.username &&
+      account.password == credentials.password
+    ) {
+      responsObject.success = true;
+
+      const cookieId = Math.round(Math.random() * 10000);
+
+      account.cookie = cookieId;
+      res.cookie("loggedIn", cookieId);
+      console.log("check account");
+      console.log(account);
+    }
+  });
+
+  res.json(responsObject);
+});
+
+app.get("/api/loggedIn", (req, res) => {
+  // to check if user is logged in to system I check it has teh cookie taht is set in login process
+  console.log("LOGGEDIN API");
+  const cookie = req.cookies.loggedIn;
+  console.log("coockie " + cookie);
+  console.log(console.log(accounts));
+
+  let responsObject = {
+    loggedIn: false,
+  };
+  accounts.forEach((account) => {
+    if (account.cookie == parseInt(cookie)) {
+      responsObject.loggedIn = true;
+    }
+  });
+  console.log(responsObject);
+  res.json(responsObject);
+});
+
+app.get("/api/logout", (req, res) => {
+  res.clearCookie("loggedIn");
+  const responsObject = {
+    success: true,
+  };
+  res.json(responsObject);
+});
+
+app.get("/api/deleteAccount", (req, res) => {
+  let cookie = req.cookies.loggedIn;
+
+  const responsObject = {
+    success: true,
+  };
+
+  accounts = accounts.filter((account) => {
+    if (account.cookie !== parseInt(cookie)) {
+      return account;
+    }
+  });
+
   console.log(accounts);
+
+  res.clearCookie("loggedIn");
   res.json(responsObject);
 });
 
